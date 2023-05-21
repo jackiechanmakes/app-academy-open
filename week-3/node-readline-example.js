43a88c42afe00f8fb60ebf99201f74f6b7984b0c
jackiechanmakes <jackiechanmakes@gmail.com>
2023-05-20 21:23:00 -0700

    Add node-readline-example.js

--- week-3/node-readline-example.js
+++ week-3/node-readline-example.js
@@ -1,1 +1,38 @@
+// Import the readline module
+const readline = require("readline");
 
+// Create an interface to talk to the user
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// Ask the user a question
+// rl.question("What's up? ", answer => {
+    // Print user response
+    // console.log("You responded: " + answer);
+    // Close the interface
+    // r1.close();
+// });
+
+// Showcases asynchronicity of question method
+// console.log("DONE");
+
+// Callback chaining with named functions instead of anonymous functions
+// for readability
+rl.question("What's up, doc? ", handleResponseOne);
+
+function handleResponseOne(firstAnswer) {
+  console.log(firstAnswer + " is up.");
+  rl.question("What's down, clown? ", handleResponseTwo);
+}
+
+function handleResponseTwo(secondAnswer) {
+  console.log(secondAnswer + " is down.");
+  rl.question("What's left, Jeff? ", handleResponseThree);
+}
+
+function handleResponseThree(thirdAnswer) {
+  console.log(thirdAnswer + " is left.");
+  rl.close();
+}
