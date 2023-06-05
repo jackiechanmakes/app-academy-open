b993c15699e06ba576390e182eab115226a221ac
jackiechanmakes <jackiechanmakes@gmail.com>
2023-06-05 13:12:01 -0700

    Write Person class

--- week-4/practice-for-week-04-class-practice/classes/person.js
+++ week-4/practice-for-week-04-class-practice/classes/person.js
@@ -1,4 +1,32 @@
-// Your code here
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  introduce() {
+    console.log(`Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`)
+  }
+
+  static introducePeople(persons) {
+    if (!Array.isArray(persons)) {
+      throw new Error("introducePeople only takes an array as an argument.");
+    }
+
+    for (let i = 0; i < persons.length; i++) {
+      let person = persons[i];
+      if (!(person instanceof Person)) {
+        throw new Error("All items in array must be Person class instances.");
+      }
+    }
+
+    for (let i = 0; i < persons.length; i++) {
+      let person = persons[i];
+      person.introduce();
+    }
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
