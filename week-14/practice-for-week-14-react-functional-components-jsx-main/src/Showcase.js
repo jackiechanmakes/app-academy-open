a6a68441dfd6f5f57099a6c943c3557da6eeb36f
jackiechanmakes <jackiechanmakes@gmail.com>
2023-09-09 17:49:34 -0700

    Add JavaScript to your JSX

--- week-14/practice-for-week-14-react-functional-components-jsx-main/src/Showcase.js
+++ week-14/practice-for-week-14-react-functional-components-jsx-main/src/Showcase.js
@@ -1,9 +1,15 @@
 function Showcase() {
-    let favPokemon = "Bulbasaur";
+    let favPokemon = 'Bulbasaur';
+
+    const pokeCharacteristics = {
+        type: 'Grass',
+        move: 'Vine Whip'
+    };
 
     return (
         <div>
             <h1>{favPokemon}'s Showcase Component</h1>
+            <h2>{favPokemon}'s type is {pokeCharacteristics['type']} and one of their moves is {pokeCharacteristics['move']}</h2>
         </div>
     );
 }
