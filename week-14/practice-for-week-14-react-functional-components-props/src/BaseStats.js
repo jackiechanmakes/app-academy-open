c7495fcb2bf8d20588fcba64aa84e2c1abbec461
jackiechanmakes <jackiechanmakes@gmail.com>
2023-09-09 20:40:07 -0700

    Add functional component

---
+++ week-14/practice-for-week-14-react-functional-components-props/src/BaseStats.js
@@ -0,0 +1,12 @@
+import "./BaseStats.css";
+
+function BaseStats() {
+  return (
+    <>
+      <div className="base-stats"></div>
+      <h1>BaseStats</h1>
+    </>
+  );
+}
+
+export default BaseStats;
