6ea88240ec437ef4f339a1227060d5a34440eb76
jackiechanmakes <jackiechanmakes@gmail.com>
2023-09-09 18:12:09 -0700

    Add inline CSS to JSX

--- week-14/practice-for-week-14-react-functional-components-jsx-main/src/Showcase.js
+++ week-14/practice-for-week-14-react-functional-components-jsx-main/src/Showcase.js
@@ -12,7 +12,9 @@ function Showcase() {
         <div>
             <h1>{favPokemon}'s Showcase Component</h1>
             <img src={BulbasaurImage} alt={favPokemon}></img>
-            <h2>{favPokemon}'s type is {pokeCharacteristics['type']} and one of their moves is {pokeCharacteristics['move']}</h2>
+            <h2>
+                {favPokemon}'s type is <span style={{  color: 'white', backgroundColor: 'green' }}>{pokeCharacteristics['type']}</span> and one of their moves is <span style= {{ color:'#008000', backgroundColor: "#FFFFFF" }}>{pokeCharacteristics['move']}</span>
+            </h2>
         </div>
     );
 }
