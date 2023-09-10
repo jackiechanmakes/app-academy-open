5889e98fca546b0b475d6b6b93d797a327c92803
jackiechanmakes <jackiechanmakes@gmail.com>
2023-09-09 20:14:58 -0700

    Reset project

--- week-14/practice-for-week-14-react-functional-components-props/src/App.js
+++ week-14/practice-for-week-14-react-functional-components-props/src/App.js
@@ -1,12 +1,10 @@
-import Showcase from './Showcase.js';
-import BaseStats from './BaseStats.js'
+import Showcase from './Showcase';
 import './App.css';
 
 function App() {
   return (
     <div className='main-wrapper background'>
       <Showcase />
-      <BaseStats />
     </div>
   );
 }
