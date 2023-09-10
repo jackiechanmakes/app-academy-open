1bf5a0d2e4fb50def2185ceb5bcc605f44a88f7e
jackiechanmakes <jackiechanmakes@gmail.com>
2023-09-09 19:02:31 -0700

    Add project

---
+++ week-14/practice-for-week-14-react-functional-components-props/src/App.js
@@ -0,0 +1,14 @@
+import Showcase from './Showcase.js';
+import BaseStats from './BaseStats.js'
+import './App.css';
+
+function App() {
+  return (
+    <div className='main-wrapper background'>
+      <Showcase />
+      <BaseStats />
+    </div>
+  );
+}
+
+export default App;
