37c49775e9d838438d3af758ff5b9447b5b921c1
jackiechanmakes <jackiechanmakes@gmail.com>
2023-09-09 21:20:01 -0700

    Use props

--- week-14/practice-for-week-14-react-functional-components-props/src/App.js
+++ week-14/practice-for-week-14-react-functional-components-props/src/App.js
@@ -3,10 +3,19 @@ import BaseStats from "./BaseStats";
 import "./App.css";
 
 function App() {
+  const baseStats = {
+    hp: 45,
+    attack: 49,
+    defense: 49,
+    spAttack: 65,
+    spDef: 65,
+    speed: 45
+  };
+
   return (
     <div className="main-wrapper background">
       <Showcase />
-      <BaseStats />
+      <BaseStats stats={baseStats}/>
     </div>
   );
 }
--- week-14/practice-for-week-14-react-functional-components-props/src/BaseStats.js
+++ week-14/practice-for-week-14-react-functional-components-props/src/BaseStats.js
@@ -1,10 +1,31 @@
 import "./BaseStats.css";
 
-function BaseStats() {
+function BaseStats(props) {
   return (
     <>
       <div className="base-stats"></div>
       <h1>BaseStats</h1>
+      <table>
+        <tbody>
+            <tr>
+                <td>Hit Points</td>
+                <td>{props.stats.hp}</td>
+            </tr>
+            <tr>
+                <td>Attack</td>
+                <td>{props.stats.attack}</td>
+            </tr>
+            <tr>
+                <td>Defense</td>
+                <td>{props.stats.defense}</td>
+            </tr>
+            <tr>
+                <td>Speed</td>
+                <td>{props.stats.speed}</td>
+            </tr>
+        </tbody>
+      </table>
+
     </>
   );
 }
