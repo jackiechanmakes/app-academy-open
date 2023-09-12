9f736c81b30eab09213c26d631fcb22172b97c9f
jackiechanmakes <jackiechanmakes@gmail.com>
2023-09-12 14:41:06 -0700

    Add list

--- week-14/practice-for-week-14-react-functional-components-lists/src/PokeMoveCard.js
+++ week-14/practice-for-week-14-react-functional-components-lists/src/PokeMoveCard.js
@@ -1,13 +1,8 @@
 import './PokeMoveCard.css'
 
-function PokeMoveCard({ id, type, level, move }) {
+function PokeMoveCard() {
     return (
-        <li className='poke-move-card'>
-            <h3>Move {id}</h3>
-            <h4 style={{ padding: 10 }}>{move.toUpperCase()}</h4>
-            <p>Type: {type}</p>
-            <p>Level: {level}</p>
-        </li>
+        <li>PokeMoveCard Component</li>
     );
 }
 
