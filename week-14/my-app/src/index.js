3920627f7cc7d90e6efd89b578e956779bb2bfea
jackiechanmakes <jackiechanmakes@gmail.com>
2023-09-19 18:04:50 -0700

    Add Link

--- week-14/my-app/src/index.js
+++ week-14/my-app/src/index.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import App from './App';
+import Users from './components/Users';
+import Profile from './components/Profile';
+
+
+const Root = () => {
+  const handleClick = () => {
+    console.log('Thanks for clicking!')
+  };
+
+  return (
+    <BrowserRouter>
+      <div>
+        <Link to="/">App</Link>
+        <Link to="/users">Users</Link>
+        <Link to="/users/1">Andrew's Profile</Link>
+        <Link to="/" onClick={handleClick}>App with click handler</Link>
+
+        <Route exact path="/">
+          <App />
+        </Route>
+        <Route exact path="/users">
+          <Users />
+        </Route>
+        <Route path="/users/:userId">
+          <Profile />
+        </Route>
+      </div>
+    </BrowserRouter>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
